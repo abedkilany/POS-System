15d218e1b13e4fa0c542c126ca75058fd1efff2d
abedkilany <abedkilany@gmail.com>
2026-06-01 06:57:48 +0300

    Ventio

--- lib/core/services/backup_download_service.dart
+++
@@ -1,9 +0,0 @@
-import 'backup_download_service_stub.dart'
-    if (dart.library.html) 'backup_download_service_web.dart' as impl;
-
-Future<void> downloadTextFile({
-  required String filename,
-  required String content,
-}) {
-  return impl.downloadTextFile(filename: filename, content: content);
-}
