7d986091958d2e3ae7755ba2e7f78b3339a8b19e
abedkilany <abedkilany@gmail.com>
2026-06-10 16:41:10 +0300

    Ventio

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ventio"
+    namespace = "com.kilany.ventio"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ventio"
+        applicationId = "com.kilany.ventio"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
