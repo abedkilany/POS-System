2bbc64dac9d61bed8cdf0c37bbd78d037388c633
abedkilany <abedkilany@gmail.com>
2026-07-12 10:46:36 +0300

    "update"

--- lib/core/app_brand.dart
+++ lib/core/app_brand.dart
@@ -4,7 +4,7 @@ class AppBrand {
   static const name = 'Ventio';
   static const version = String.fromEnvironment(
     'APP_VERSION',
-    defaultValue: '1.0.11+11',
+    defaultValue: '1.0.12+12',
   );
   static const cloudAppVersion = 'ventio';
   static const description =
